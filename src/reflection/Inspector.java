5cb192224840f330d0e2cc1a89a4e075d42c5859
Mona Agh <mona.agh1@ucalgary.ca>
2019-10-25 18:13:07 -0600

    recursion on fields and arrays added. Unit testing added

--- src/reflection/Inspector.java
+++ src/reflection/Inspector.java
@@ -13,59 +13,110 @@ public class Inspector {
     }
 
     private void inspectClass(Class c, Object obj, boolean recursive, int depth) {
-    	//System.out.println(c.getName());
+
     	int d = depth;
-    	//Class superclassObject = c.getSuperclass();
     	int count = 0;
     	int dep = depth;
     	
     	//while (!c.equals(Object.class) && count == 0) {
     	//while (count == 0) {
-    		if (c.isArray()) {
-    			System.out.println(c.getName() + " is an array");
-    			//TO DO
-    		} else {
-	    		if (c.equals(Object.class)) {
-	    			count++;
-	    		}
-	    		//System.out.println("/////////////////// " + count + "///////////////");
-	    		System.out.println();
-	    		printIndent(d);
-	    		System.out.println("***************Inspection for Class '" + c.getName()+ "***************\n");
-	    		getClassName(c, d);
-	    		
-	    		//inspectSuperClass(c, d);    		
-	    		if (count != 1) {
-	    			
-	    			printIndent(dep);
-	    	    	System.out.println("--------------Inspecting Immediate Superclass--------------");
-	    	    	
-	    			printIndent(dep);
-	    			System.out.println("Superclass name:" + c.getSuperclass().getName());
-	    			dep = dep +1;
-	    			inspectClass(c.getSuperclass(), obj, recursive, dep);	
-	    		}
-	    		
-	    		//printIndent(d);
-	    		//System.out.println("***************Continue Inspection for Class '" + c.getName()+ "***************\n");
-	    		
-	    		inspectInterface(c, obj, d);
-	    		inspectConstructor(c, d);
-	    		inspectMethod(c,d);
-	    		inspectField(c, obj, d);
-	    		//c = c.getSuperclass();
-	    		d++;
-    		}
-    		   		
-    	}
     	
+	    if (c.equals(Object.class)) {
+	    	count++;
+  		}
+	    //System.out.println("/////////////////// " + count + "///////////////");
+		System.out.println();
+		printIndent(d);
+		System.out.println("***************Inspection for Class '" + c.getName()+ "***************\n");
+	    getClassName(c, d);
+	    		
+	    //inspectSuperClass(c, d);    		
+	  	if (count != 1) {
+	    			
+	    	printIndent(dep);
+	    	System.out.println("--------------Inspecting Immediate Superclass--------------");
+	    	    	
+	    	printIndent(dep);
+	    	System.out.println("Superclass name:" + c.getSuperclass().getName());
+	    	dep = dep +1;
+	    	inspectClass(c.getSuperclass(), obj, recursive, dep);	
+	    }
+	    		
+	  	//printIndent(d);
+	    //System.out.println("***************Continue Inspection for Class '" + c.getName()+ "***************\n");
+	    		
+	    inspectInterface(c, obj, d, recursive);
+	    inspectConstructor(c, d);
+	    inspectMethod(c,d);
+	    inspectField(c, obj, d, recursive);
+	    //c = c.getSuperclass();
+	    		
+	    if (c.isArray()) {
 
+	    	printIndent(depth);
+	    	System.out.println("  Array Name: " + c.getName());
+	    			
+	    	//array component type
+	    	printIndent(depth);
+	    	System.out.println("  Array Component Type: " +  c.getComponentType().getName());
+	    			
+	    	//try{
+	    				//array length
+	    	printIndent(depth);
+	    	//c.setAccessible(true);
+	    				
+	    	int length= Array.getLength(obj);
+	    	System.out.println("  Array Length: " + length);
+	    	//System.out.println(field.get(c.newInstance()));
+	    				
+	    	//array content
+	    	printIndent(depth);
+	    	System.out.print("  Array Content: ");
+	    	for (int i = 0; i< length; i++) {    
+	    		
+	    		Object arrayElement = Array.get(obj,  i);	
+	    		//System.out.print(Array.get(field.get(c.newInstance()), i) + " " );
+	    		System.out.print(arrayElement + " ");
+	    		if (arrayElement != null) {
+		    		if (recursive) {
+						
+						Class arrayElementClass = arrayElement.getClass();
+						
+						System.out.println();
+						printIndent(d);
+						System.out.println("***************Inspection for element " + i + ": "+ arrayElementClass.getName() + " with type "
+						+ arrayElementClass + " ***************");
+						
+						inspectClass(arrayElementClass, arrayElement, recursive, d);
+						System.out.println();
+						printIndent(d);
+						System.out.println("***************Finished Inspection for element " + i + ": "+ arrayElement.getClass().getName()+ "***************\n");
+					}
+	    		}
+	    	}
+	    					
+	    }
+	    
+	    System.out.println();
+		printIndent(d);
+		System.out.println("***************Finished Inspection for Class '" + c.getName()+ "***************\n");
+		d++;
+    }
+
+    
+    	    
     private void inspectConstructor(Class c, int depth) {
     	int index=0;
     	//get all constructors
     	Constructor[] allConstructors = c.getDeclaredConstructors();
     	printIndent(depth);
     	System.out.println("--------------Inspecting Declared Constructors--------------");
+    	
+    	if (allConstructors.length == 0) {
+    		printIndent(depth);
+    		System.out.println("No Constructors");
+    	}
+    	
     	// print information for all constructors
     	for (Constructor cons: allConstructors) {
     		
@@ -105,6 +156,10 @@ public class Inspector {
     	printIndent(depth);
     	System.out.println("--------------Inspecting Declared Methods--------------");
     	
+    	if (methods.length == 0) {
+    		printIndent(depth);
+    		System.out.println("No Methods");
+    	}
     	// get information for each method
     	for (Method method: methods) {
     		printIndent(depth);
@@ -151,8 +206,9 @@ public class Inspector {
     	}
     }
     
-    private void inspectField(Class c, Object obj, int depth) {
+    private void inspectField(Class c, Object obj, int depth, boolean recursive) {
     	int index = 0;
+    	int d = depth+1;
     	//get all fields
     	Field[] fields = c.getDeclaredFields();
     	
@@ -182,18 +238,19 @@ public class Inspector {
     		printIndent(depth);
     		System.out.println(" Field modifer: " + getModifierString(field.getModifiers()));
     		
+    		field.setAccessible(true);
+			Object value = null; 
+			try {
+				value = field.get(obj);
+			} catch (IllegalAccessException e) {
+				System.out.println(e);
+			}
+
     		//field current value
     		if (!field.getType().isArray()) {
-    			field.setAccessible(true);
-    			Object value = null; 
-    			try {
-    				value = field.get(obj);
-    			} catch (IllegalAccessException e) {
-    				System.out.println(e);
-    			}
-    			printIndent(depth);
     			
-    				
+    			
+    			printIndent(depth);
     				// if a field is primitive
     			if (field.getType().isPrimitive()) {
     				//Object value = field.get(obj);
@@ -202,121 +259,122 @@ public class Inspector {
     				
     			// if field is a reference object
    				else {
-   					
-   					//Object value = field.get(obj);
    					int hashcode = System.identityHashCode(field);
    					String reference = Integer.toHexString(hashcode);
    					if (value != null) {
     					System.out.println(" Field Value: " + value.getClass().getName() + "@" + reference);
-    				} else {
+    					
+    					//recursion on fields
+    					if (recursive) {
+	    					System.out.println();
+	    					printIndent(d);
+	    					System.out.println("***************Inspection for field " + field.getName() + " with type "
+	    					+ field.getType() + " ***************");
+	    					Class fieldClass = value.getClass();
+	    					inspectClass(fieldClass, value, recursive, d);
+	    					System.out.println();
+	    					printIndent(d);
+	    					System.out.println("***************Finished Inspection for field " + field.getName()+ "***************\n");
+    					}
+   					
+   					} else {
     					System.out.println(" Field value: null");
     				}
    				}
     		} else {
     			
-    			//Class array = field.getType();
-    			//Object arrayObject
     			//array name
     			printIndent(depth);
-    			System.out.println("  Array Name: " + field.getType().getName());
+    			System.out.println("  Array Name: " + field.getName());
     			
     			//array component type
     			printIndent(depth);
     			System.out.println("  Array Component Type: " +  field.getType().getComponentType().getName());
-    			
-    			try {
-    				//array length
-    				printIndent(depth);
-    				field.setAccessible(true);
-    				int length= Array.getLength(field.get(c.newInstance()));
-    				System.out.println("  Array Length: " + length);
+
+    			printIndent(depth);
+    			field.setAccessible(true);
     				
-    				//array content
-    				printIndent(depth);
-    				System.out.print("  Array Content: ");
-    				for (int i = 0; i< length; i++) {
-    					
-    					System.out.print(Array.get(field.get(c.newInstance()), i) + " " );
+    			int length= Array.getLength(value);
+    			System.out.println("  Array Length: " + length);
+    			//System.out.println(field.get(c.newInstance()));
+    				
+    			//array content
+    			printIndent(depth);
+    			System.out.print("  Array Content: ");
+    			for (int i = 0; i< length; i++) {
+    				Object arrayElement = Array.get(value,  i);	
+    				//System.out.print(Array.get(field.get(c.newInstance()), i) + " " );
+    				System.out.print(arrayElement + " ");
+    				if (arrayElement != null) {
+    					//recursion on elements
+    					if (recursive) {
+    						
+    						Class arrayElementClass = arrayElement.getClass();
+    						
+	    					System.out.println();
+	    					printIndent(d);
+	    					System.out.println("***************Inspection for element "+ i + ": " + arrayElementClass.getName() + " with type "
+	    					+ arrayElementClass + " ***************");
+	    					
+	    					inspectClass(arrayElementClass, arrayElement, recursive, d);
+	    					System.out.println();
+	    					printIndent(d);
+	    					System.out.println("***************Finished Inspection for element " + i + ": "+ arrayElement.getClass().getName()+ "***************\n");
+    					}
     				}
-    				
-    				System.out.println();
-    			} catch (IllegalAccessException e) {
-    				System.out.println(e);
-    			} catch (InstantiationException e) {
-    				System.out.println(e);
     			}
+    				
+    			System.out.println();
+    			
+    			
     		}
     	}
     }
     
     
-	private void inspectInterface(Class c, Object obj, int depth) {
-		int d = depth;
-		//Object obj = null;
-		//Array allInterfaces;
+	private void inspectInterface(Class c, Object obj, int depth, boolean recursive) {
+		//depth++;
+		int d = depth+1;
+
 		
 		printIndent(depth);
     	System.out.println("--------------Inspecting Implemented Interfaces--------------");
     	
-    	
-		//printIndent(depth);
-		//System.out.println("Interfaces: ");
+
 		Class[] interfaces = c.getInterfaces();
-		//allInterfaces.add(interfaces);
 		if (interfaces.length != 0) {
 			for (int i = 0; i < interfaces.length; i++ ) {
-				printIndent(d);
+				printIndent(depth);
 				System.out.println("Interface #" + i);
 				
-				printIndent(d);
+				printIndent(depth);
 				System.out.println(" Interface name: " + interfaces[i].getName());
 				
-	    		d++;
+	    		//depth++;
 	    		System.out.println();
 				printIndent(d);
 	    		System.out.println("***************Inspection for Interface '" + interfaces[i].getName()+ "'***************\n");
 	    		
-	    		inspectInterface(interfaces[i], obj, d);
+	    		//d++;
+	    		inspectInterface(interfaces[i], obj, d, recursive);
 	    		
 	    		System.out.println();
 	    		printIndent(d);
 	    		System.out.println("***************Continue Inspection for Interface '" + interfaces[i].getName()+ "'***************\n");
 	    		
 	    		inspectMethod(interfaces[i], d);
-	    		inspectField(interfaces[i], obj, d);
+	    		inspectField(interfaces[i], obj, d, recursive);
 	    		
 	    		System.out.println();
 	    		printIndent(d);
 	    		System.out.println("***************Finished Inspection for Interface '" + interfaces[i].getName()+ "'***************\n");
 				
-				
-				
-				//allInterfaces.add(interfaces[i]);	
 			}
 		} else {
 			printIndent(depth);
 			System.out.println("No Interface");
 		}
 		
-		/*if (interfaces.length != 0) {
-			for (int i = 0; i < interfaces.length; i++ ) {
-				printIndent(depth);
-				System.out.println(" Interface name: " + interfaces[i].getName());
-				try {
-					obj = interfaces[i].newInstance();
-					inspectClass(interfaces[i], obj, recursive, depth);
-				} catch (InstantiationException e) {
-					System.out.println(e);
-				} catch (IllegalAccessException e) {
-					System.out.println(e);
-				}
-				
-				
-			}
-		} else {
-			printIndent(depth);
-			System.out.println("No Interface");
-		}*/
 	}
 
 	private void inspectSuperClass(Class c, int depth) {
--- src/test/TestInspector.java
+++ src/test/TestInspector.java
@@ -9,7 +9,9 @@ import org.junit.After;
 public class TestInspector {
 	ClassA a;
 	ClassB[] bArray;
+	ClassB[][] bArray2;
 	ClassB b;
+	ClassD d;
 	String str;
 	Boolean recursive;
 	
@@ -21,13 +23,102 @@ public class TestInspector {
 		
 	}
 	
-	@Test
-	public void TestClassName() throws Exception {
+	/*@Test
+	public void TestClassA() throws Exception {
+		recursive = true;
 		a = new ClassA();
+		
+		bArray2 = new ClassB[12][12];
+		
+		new Inspector().inspect(a, recursive);
+	}
+	
+	@Test
+	public void TestClassAWithParameter() throws Exception {
+		recursive = true;
+		a = new ClassA(12);
+		new Inspector().inspect(a, recursive);
+	}
+	
+	@Test
+	public void TestClassB() throws Exception {
+		recursive = true;
 		b = new ClassB();
-		bArray = new ClassB[12];
-		str = "Test String";
 		new Inspector().inspect(b, recursive);
 	}
+	
+	@Test
+	public void TestClassD() throws Exception {
+		recursive = true;
+		d = new ClassD();
+		new Inspector().inspect(d, recursive);
+	}
+	
+	@Test
+	public void TestClassDWithParameter() throws Exception {
+		recursive = true;
+		d = new ClassD(32);
+		new Inspector().inspect(d, recursive);
+	}
+	
+	@Test
+	public void TestClassDWith() throws Exception {
+		recursive = true;
+		d = new ClassD(32);
+		new Inspector().inspect(d, recursive);
+	}*/
+	
+	/*@Test
+	public void TestClassString() throws Exception {
+		recursive = true;
+		str = "Test String";
+		new Inspector().inspect(str, recursive);
+	}*/
+	
+	/*@Test
+	public void TestClass1DArray() throws Exception {
+		recursive = true;
+		bArray = new ClassB[12];
+		new Inspector().inspect(bArray, recursive);
+	}*/
+	
+	@Test
+	public void TestClass2DArray() throws Exception {
+		recursive = true;
+		bArray2 = new ClassB[12][12];
+		new Inspector().inspect(bArray2, recursive);
+	}
+	
+	
+	/*@Test
+	public void TestArrayField() throws Exception {
+		//recursive = true;
+		class ClassA {
+			private double[] arr = {2.3, 2.4};
+			private String str;
+			private int number; 
+			
+			public ClassA() {
+				str = "Test";
+			}
+			
+			public ClassA(int i) {
+				number = i;
+				str = "Test";
+			}
+			
+		};
+		
+		ClassA a = new ClassA();
+		new Inspector().inspect(a, recursive);
+	}
+	
+	/*@Test
+	public void TestArrayObject()	 {
+		recursive = true;
+		double[] arr = {2.3, 2.4};
+		new Inspector().inspect(arr, recursive);
+	}*/
+	
 	
 }
